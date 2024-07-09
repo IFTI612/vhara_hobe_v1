505a1a648c65391032ef0fb7b34869ac884ba0aa
MoonRise <152148710+MoonRise1080@users.noreply.github.com>
2024-07-09 23:12:42 +0600

    Messenger start

---
+++ lib/src/features/authentication/screens/messenger.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class messenger extends StatefulWidget {
+  const messenger({super.key});
+
+  @override
+  State<messenger> createState() => _messenger();
+}
+
+class _messenger extends State<messenger> {
+
+  Widget build(BuildContext context){
+    return(
+
+    );
+  }
+}
